d82598c0759a53296d5ab1f251e149a090489917
Robert2003 <47471127+Robert2003@users.noreply.github.com>
2019-11-28 21:37:18 +0200

    Merge branch 'master' of E:\GitHub\FTCTeaBorgsRobot2020 with conflicts.

--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/BasicOpMode_Linear.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/BasicOpMode_Linear.java
@@ -85,7 +85,6 @@ public class BasicOpMode_Linear extends LinearOpMode {
             // Setup a variable for each drive wheel to save power level for telemetry
             double leftPower;
             double rightPower;
-
             // Choose to drive using either Tank Mode, or POV Mode
             // Comment out the method that's not used.  The default below is POV.
 
