7e1e7702d12cfec9c952c27645ad8966b61edcaf
Robert2003 <47471127+Robert2003@users.noreply.github.com>
2019-12-09 20:15:13 +0200

    Merge branch 'master' of E:\GitHub\FTCTeaBorgsRobot2020 with conflicts.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VuforiaTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VuforiaTest.java
@@ -1,9 +1,15 @@
 package org.firstinspires.ftc.teamcode;
 
+import android.graphics.Bitmap;
+import android.graphics.Camera;
+import android.nfc.Tag;
+
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.vuforia.CameraDevice;
 import com.vuforia.HINT;
+import com.vuforia.Image;
+import com.vuforia.PIXEL_FORMAT;
 import com.vuforia.Vuforia;
 
 import org.firstinspires.ftc.robotcore.external.ClassFactory;
@@ -20,10 +26,11 @@ public class VuforiaTest extends LinearOpMode
 {
     private HardwareRobot func = null;
 
+    VuforiaLocalizer.CloseableFrame closeableFrame = new VuforiaLocalizer.CloseableFrame();
+
     @Override
     public void runOpMode() throws InterruptedException
     {
-
         int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
         VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters(cameraMonitorViewId);
         parameters.cameraDirection = VuforiaLocalizer.CameraDirection.BACK;
@@ -44,6 +51,7 @@ public class VuforiaTest extends LinearOpMode
         //waitForStart();
 
         allTargets.activate();
+        CameraDevice.getInstance().setFlashTorchMode(true);
 
         func = new HardwareRobot();
         func.initFromMap(hardwareMap);
@@ -55,6 +63,21 @@ public class VuforiaTest extends LinearOpMode
             for(VuforiaTrackable target : allTargets)
             {
                 OpenGLMatrix pose = ((VuforiaTrackableDefaultListener)target.getListener()).getUpdatedRobotLocation();
+
+                long num = closeableFrame.getNumImages();
+                for(int i=0; i<num; i++)
+                {
+                    if(closeableFrame.getImage(i).getFormat() == PIXEL_FORMAT.RGB565)
+                    {
+                        Image rgb = closeableFrame.getImage(i);
+                        double width = rgb.getWidth();
+                        double height =rgb.getHeight();
+
+                        Bitmap bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.RGB_565);
+                        bitmap.copyPixelsFromBuffer(rgb.getPixels());
+
+                    }
+                }
 
                 if(pose != null)
                 {
